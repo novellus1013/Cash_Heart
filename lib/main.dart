1c16282be570c7aff68a8e8e4e54b25b4390c84d
novellus1013 <novellus1013@gmail.com>
2024-10-31 11:19:07 +0900

    firebase 기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:cash_heart/features/auth/intro_screen.dart';
+import 'package:cash_heart/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
